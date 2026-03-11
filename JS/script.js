a89a3320566adc85940de2f8f422a42c5f157846
NKbharath <bharathnk2004@gmail.com>
2026-03-11 16:08:56 +0530

    JS 1

---
+++ JS/script.js
@@ -0,0 +1,149 @@
+var age = 21;
+console.log(age);
+age = 22;
+console.log(age);
+
+
+let name = "bharath";
+console.log(name);
+name = "bharath kumar";
+console.log(name);
+
+const yr = 2004;
+console.log(yr);
+// yr = 2005; // This will throw an error because 'yr' is a constant and cannot be reassigned.
+
+var a = 10;
+console.log(a);
+var a = 20;
+console.log(a);
+
+let b = "hello";
+console.log(b);
+// let b = "world"; // This will throw an error because 'b' has already been declared in the same scope.
+
+console.log("Bharath");
+console.log(22);
+console.log("Namakkal");
+console.log(100);
+console.log("Javascript is fun");
+
+
+alert("Welcome to JavaScript!");
+
+var code = confirm("Do you like to code");
+console.log(code);
+
+var name11 = prompt("What is your name?");
+console.log(name11);
+
+
+console.warn("This is a warning");
+
+console.error("This is an error");
+
+console.log("This will be cleared");
+console.clear();
+
+let userName = prompt("What is your name?");
+alert("Your name is: " + userName);
+console.log("Name: " + userName);
+
+var userAge = prompt("What is your age?");
+console.log("Age: " + userAge);
+document.writeln("Age: " + userAge + "<br>");
+
+const hadBreakfast = confirm("Have you had breakfast?");
+console.log("Had breakfast: " + hadBreakfast);
+
+var city = "Mumbai";
+let country = "India";
+const planet = "Earth";
+console.log("City: " + city);
+console.log("Country: " + country);
+console.log("Planet: " + planet);
+
+let dynamicValue = 5;
+console.log("Value 1: " + dynamicValue);
+dynamicValue = 10;
+console.log("Value 2: " + dynamicValue);
+dynamicValue = 15;
+console.log("Value 3: " + dynamicValue);
+
+console.log("This is a normal message");
+console.warn("This is a warning message");
+console.error("This is an error message");
+
+const userFullName = prompt("What is your name?");
+let userFullAge = prompt("What is your age?");
+var isStudent = confirm("Are you a student?");
+console.log("Name: " + userFullName);
+console.log("Age: " + userFullAge);
+console.log("Is Student: " + isStudent);
+alert("Welcome " + userFullName + "!");
+
+console.clear();
+alert("Hello!");
+let task21Name = prompt("What is your name?");
+alert("Welcome " + task21Name);
+document.writeln("Welcome " + task21Name + "<br>");
+
+{
+    var blockVarA = 100;
+    let blockLetB = 200;
+    const blockConstC = 300;
+}
+console.log("Outside block - var a: " + blockVarA);
+// console.log("Outside block - let b: " + blockLetB); // This will throw an error
+// console.log("Outside block - const c: " + blockConstC); // This will throw an error
+
+var num1 = 10;
+let num2 = 20;
+const num3 = 30;
+console.log("Numbers: " + num1 + ", " + num2 + ", " + num3);
+
+var jsLikeResponse = confirm("Do you like JavaScript?");
+console.log("Like JavaScript: " + jsLikeResponse);
+console.warn("Like JavaScript: " + jsLikeResponse);
+console.error("Like JavaScript: " + jsLikeResponse);
+
+const task25Name = prompt("What is your name?");
+document.writeln("Your name is: " + task25Name + "<br>");
+console.log("Your name is: " + task25Name);
+
+let task26Age = prompt("What is your age?");
+alert("Your age is: " + task26Age);
+console.log("Your age is: " + task26Age);
+document.writeln("Your age is: " + task26Age + "<br>");
+
+if (true) {
+    var varVariable = "I am var";
+    let letVariable = "I am let";
+}
+console.log("var outside block: " + varVariable); // This works
+// console.log("let outside block: " + letVariable); // This will throw an error
+
+var isAdult = confirm("Are you 18+?");
+if (isAdult) {
+    console.log("Adult");
+} else {
+    console.warn("Minor");
+}
+
+var fruitVar = "Apple";
+fruitVar = "Banana";
+let vehicleLet = "Car";
+vehicleLet = "Bike";
+console.log("Fruit: " + fruitVar);
+console.log("Vehicle: " + vehicleLet);
+
+console.clear();
+alert("Practice Start");
+const task30Name = prompt("What is your name?");
+let task30Age = prompt("What is your age?");
+var task30Happy = confirm("Are you happy?");
+console.log("Name: " + task30Name);
+console.log("Age: " + task30Age);
+console.log("Happy: " + task30Happy);
+alert("Practice End");
+document.writeln("Thank You!");
