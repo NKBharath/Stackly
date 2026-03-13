337d8bf83eb343ebbe6906a3f0ad2b0986412142
NKbharath <bharathnk2004@gmail.com>
2026-03-13 11:41:14 +0530

    day 2

---
+++ JS/day2.js
@@ -0,0 +1,181 @@
+// Task 1
+let name = "Naveen";
+console.log(name);
+
+// Task 2
+console.log(typeof "JavaScript");
+
+// Task 3
+let age = 22;
+console.log(typeof age);
+
+// Task 4
+let isStudent = true;
+console.log(typeof isStudent);
+
+// Task 5
+let salary;
+console.log(salary);
+
+
+// Task 6
+let fruits = ["Apple","Banana","Mango","Orange","Grapes"];
+console.log(fruits);
+
+// Task 7
+console.log(fruits[0]);
+
+// Task 8
+console.log(fruits[2]);
+
+// Task 9
+console.log(fruits[fruits.length - 1]);
+
+// Task 10
+let numbers = [10,20,30,40,50];
+console.log(numbers[1]);
+console.log(numbers[3]);
+
+
+// Task 11
+let person = {
+    name: "John",
+    age: 25,
+    city: "Chennai"
+};
+console.log(person);
+
+// Task 12
+console.log(person.name);
+
+// Task 13
+person.skills = ["HTML","CSS","JS"];
+console.log(person.skills[1]);
+
+// Task 14
+let carDetails = {
+    brand: "Toyota",
+    model: "Innova",
+    price: 2000000
+};
+console.log(carDetails.brand);
+
+// Task 15
+let fruitCategory = {
+    red: ["apple","cherry"],
+    yellow: ["banana","mango"],
+    green: ["kiwi","grapes"]
+};
+console.log(fruitCategory.yellow[0]);
+
+
+// Task 16
+let a = 8;
+let b = 4;
+console.log(a + b);
+
+// Task 17
+console.log(a - b);
+
+// Task 18
+console.log(a * b);
+
+// Task 19
+console.log(a / b);
+
+// Task 20
+console.log(a % b);
+
+
+// Task 21
+console.log(3 ** 2);
+
+// Task 22
+console.log(2 ** 4);
+
+
+// Task 23
+let num = 5;
+num++;
+console.log(num);
+
+// Task 24
+let num1 = 10;
+num1--;
+console.log(num1);
+
+// Task 25
+let x = 6;
+let y = x++;
+console.log(x);
+console.log(y);
+
+// Task 26
+let a1 = 4;
+let b1 = ++a1;
+console.log(a1);
+console.log(b1);
+
+
+// Task 27
+let value = 10;
+value += 5;
+console.log(value);
+
+// Task 28
+let value1 = 20;
+value1 -= 3;
+console.log(value1);
+
+// Task 29
+let value2 = 6;
+value2 *= 2;
+console.log(value2);
+
+// Task 30
+let value3 = 20;
+value3 %= 3;
+console.log(value3);
+
+
+// Task 31
+console.log(10 == "10");
+
+// Task 32
+console.log(10 === "10");
+
+// Task 33
+console.log(15 > 20);
+
+// Task 34
+console.log(5 <= 5);
+
+
+// Task 35
+console.log(10 > 5 && 5 > 2);
+
+// Task 36
+console.log(10 < 5 || 8 > 2);
+
+// Task 37
+console.log(!(5 === "5"));
+
+
+// Task 38
+let age2 = 20;
+console.log(age2 >= 18 ? "Adult" : "Minor");
+
+// Task 39
+let password = 1;
+console.log(password === 1 ? "Login success" : "Wrong password");
+
+
+// Task 40
+let firstName = "Spider";
+let lastName = "Man";
+
+// Concatenation
+console.log(firstName + " " + lastName);
+
+// Template string
+console.log(`${firstName} ${lastName}`);
